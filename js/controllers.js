e64c26e8573055b6c6ecec6ef2b7b7d4618fef02
David <dmwhite@gmail.com>
2015-10-03 00:04:07 -0400

    wire buttons up in controller, fire tracers

--- js/controllers.js
+++ js/controllers.js
@@ -20,39 +20,42 @@
 
   // Controller for "next move"...
   $("#next").on('click', function(){
-    // console.log("clicked next move button");
+    console.log("clicked next move button");
     game.next();
-    // TODO: Tell the Model -- `game` -- to advance to the next move...
+    console.log(game.tracer());
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   // Controller for "previous move"...
   $("#back").on('click', function(){
-    // console.log("clicked on the prev move button");
+    console.log("clicked on the prev move button");
     game.prev();
-    // TODO: Tell the Model -- `game` -- to advance to the previous move...
+    console.log(game.tracer());
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   // Controller for "fast-forward"...
   $("#end").on('click', function(){
-    // console.log("clicked on the last move button");
+    console.log("clicked on the last move button");
     game.end();
-    // TODO: Tell the Model -- `game` -- to advance to the last move...
+    console.log(game.tracer());
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   // Controller for "rewind"...
   $("#reset").on('click', function(){
-    // console.log("clicked on the reset button");
+    console.log("clicked on the reset button");
     game.reset();
+    console.log(game.tracer());
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   //controller for "play/pause"
   $("#play").on('click', function(){
-    // console.log("clicked on the play/pause button");
-    game.play();
+    console.log("clicked on the play/pause button");
+    // game.play(); //TODO: add the play/pause feature
+    game.end();
+    console.log(game.tracer());
   });
 
 
