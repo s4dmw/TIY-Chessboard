2962900eecd815e5419771ad10a8658dcd201616
David <dmwhite@gmail.com>
2015-10-04 10:42:08 -0400

    changed the model to grab the moves from games.json

--- js/main.js
+++ js/main.js
@@ -21,23 +21,25 @@
    * @see applyMove
    * @var {Array} of...?
    */
-   var moves = [
-     {from:{rank: 6, file: 3}, to:{rank: 4, file: 3}},
-     {from:{rank: 0, file: 6}, to:{rank: 2, file: 5}},
-     {from:{rank: 6, file: 2}, to:{rank: 4, file: 2}},
-     {from:{rank: 1, file: 4}, to:{rank: 2, file: 4}},
-     {from:{rank: 6, file: 6}, to:{rank: 5, file: 6}},
-     {from:{rank: 1, file: 3}, to:{rank: 3, file: 3}},
-     {from:{rank: 7, file: 5}, to:{rank: 6, file: 6}},
-     {from:{rank: 0, file: 5}, to:{rank: 1, file: 4}},
-     {from:{rank: 7, file: 6}, to:{rank: 5, file: 5}}
-   ]; //end moves
+   //trying use games.json to get the moves
+   var moves
+   $.ajax("games.json").then(function(){
+    moves = arguments[0].moves});
+    
+
+  //  var moves = [
+  //    {from:{rank: 6, file: 3}, to:{rank: 4, file: 3}},
+  //    {from:{rank: 0, file: 6}, to:{rank: 2, file: 5}},
+  //    {from:{rank: 6, file: 2}, to:{rank: 4, file: 2}},
+  //    {from:{rank: 1, file: 4}, to:{rank: 2, file: 4}},
+  //    {from:{rank: 6, file: 6}, to:{rank: 5, file: 6}},
+  //    {from:{rank: 1, file: 3}, to:{rank: 3, file: 3}},
+  //    {from:{rank: 7, file: 5}, to:{rank: 6, file: 6}},
+  //    {from:{rank: 0, file: 5}, to:{rank: 1, file: 4}},
+  //    {from:{rank: 7, file: 6}, to:{rank: 5, file: 5}}
+  //  ]; //end moves
 
 
-   //
-  // var moves = [[6, 3, 4, 3], [0, 6, 2, 5], [6, 2, 4, 2], [1, 4, 2, 4],
-  //   [6, 6, 5, 6], [1, 3, 3, 3], [7, 5, 6, 6], [0, 5, 1, 4], [7, 6, 5, 5]
-  // ]; // END moves
 
 
   // You don't need to understand `globals` yet...
