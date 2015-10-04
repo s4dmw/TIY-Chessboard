d34af45df0b0b2e14b3b8e1a3419bfff80374a05
David <dmwhite@gmail.com>
2015-10-04 13:01:02 -0400

    created menu.js to populate my opening selection menu

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+(function(globals){ //IIFE y0!
+
+  /** the purpose of this file is to populate the "choose and opening" menu
+  * with the games from /apis/games.json...here goes nuffin
+  */
+
+  // console.log("menu.js loaded and ready for action"); //tracer to make sure it's loaded
+
+
+
+
+
+})(window || module && module.exports || this); //end that IIFFE tho
